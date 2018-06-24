e8f03c08d2710dc2a96cc6016712fb690907a1a8
NERD <smallnerd.k@gmail.com>
2018-06-24 19:52:24 +0800

    将代码从test中移到主目录下

--- demo/L298NHBridge.py
+++ demo/L298NHBridge.py
@@ -1,150 +1,161 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# Author:
-# Data:
-# Version:
-#Thanks for origin Author's kevin
+# Autor:        孔NERD
+# Date:         2018-6-22
+# VersGPIOn:      1.0
+#Thanks for origin Autor's Ingmar Stape and kevin
 
 # This module is designed to control two motors with a L298N H-Bridge
 
-# Use this module by creating an instance of the class. To do so call the Init function, then command as desired, e.g.
+# Use this module by creating an instance of the class. To do so call the Init functGPIOn, then command as desired, e.g.
 # import L298NHBridge
 # HBridge = L298NHBridge.L298NHBridge()
 # HBridge.Init()
 
 # Import the libraries the class needs
-import RPi.GPIO as io
+import RPi.GPIO as GPIO
 import time
 
 class HBridge(object):
 
-    def __init__(self, motor1_pin1, motor1_pin2, motor2_pin1, motor2_pin2, motor1pwm_pin, motor2pwm_pin):
-        io.setmode(io.BCM)
+    def __init__(self, motor1_pin1, motor1_pin2, motor2_pin1, motor2_pin2, motor1pwm_pin,
+            motor2pwm_pin, steerpwm_pin):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
         # Constant values
         self.PWM_MAX = 100
-        # Here we configure the GPIO settings for the left and right motors spinning direction.
-        # It defines the four GPIO pins used as input on the L298 H-Bridge to set the motor mode (forward, reverse and stopp).
+        self.LEFT_MAX = 6.5
+        self.RIGHT_MAX = 20.5
+        self.CENTER = 13.5
+        # Here we configure the GPGPIO settings for the left and right motors spinning directGPIOn.
+        # It defines the four GPGPIO pins used as input on the L298 H-Bridge to set the motor mode (forward, reverse and stopp).
         self.motor1_in1_pin = motor1_pin1
         self.motor1_in2_pin = motor1_pin2
         self.motor2_in1_pin = motor2_pin1
         self.motor2_in2_pin = motor2_pin2
         self.motor1pwm_pin = motor1pwm_pin
         self.motor2pwm_pin = motor2pwm_pin
+        self.steerpwm_pin = steerpwm_pin
         self.SetupGPIO()
-        self.motor1pwm = io.PWM(self.motor1pwm_pin,100)
-        self.motor2pwm = io.PWM(self.motor2pwm_pin,100)
+        self.motor1pwm = GPIO.PWM(self.motor1pwm_pin, 1000)
+        self.motor2pwm = GPIO.PWM(self.motor2pwm_pin, 1000)
+        self.steerpwm = GPIO.PWM(self.steerpwm_pin, 100)
         self.InitPWM()
         # Disable warning from GPIO
-        io.setwarnings(False)
 
     def SetupGPIO(self):
-        io.setup(self.motor2_in1_pin, io.OUT)
-        io.setup(self.motor2_in2_pin, io.OUT)
-        io.setup(self.motor1_in1_pin, io.OUT)
-        io.setup(self.motor1_in2_pin, io.OUT)
-        io.setup(self.motor1pwm_pin, io.OUT)
-        io.setup(self.motor2pwm_pin, io.OUT)
+        GPIO.setup(self.motor2_in1_pin, GPIO.OUT)
+        GPIO.setup(self.motor2_in2_pin, GPIO.OUT)
+        GPIO.setup(self.motor1_in1_pin, GPIO.OUT)
+        GPIO.setup(self.motor1_in2_pin, GPIO.OUT)
+        GPIO.setup(self.motor1pwm_pin, GPIO.OUT)
+        GPIO.setup(self.motor2pwm_pin, GPIO.OUT)
+        GPIO.setup(self.steerpwm_pin, GPIO.OUT)
+        
 
     def InitPWM(self):
-        # Here we configure the GPIO settings for the left and right motors spinning speed.
-        # It defines the two GPIO pins used as input on the L298 H-Bridge to set the motor speed with a PWM signal.
+        # Here we configure the GPGPIO settings for the left and right motors spinning speed.
+        # It defines the two GPGPIO pins used as input on the L298 H-Bridge to set the motor speed with a PWM signal.
         self.motor1pwm.start(0)
         self.motor1pwm.ChangeDutyCycle(0)
         self.motor2pwm.start(0)
         self.motor2pwm.ChangeDutyCycle(0)
+        self.steerpwm.start(0)
+        #self.steerpwm.ChangeDutyCycle(0)
 
     def resetMotorGPIO(self):
-        io.output(self.motor1_in1_pin, False)
-        io.output(self.motor1_in2_pin, False)
-        io.output(self.motor2_in1_pin, False)
-        io.output(self.motor2_in2_pin, False)
+        GPIO.output(self.motor1_in1_pin, False)
+        GPIO.output(self.motor1_in2_pin, False)
+        GPIO.output(self.motor2_in1_pin, False)
+        GPIO.output(self.motor2_in2_pin, False)
 
 # setMotorMode()
 
 # Sets the mode for the L298 H-Bridge which motor is in which mode.
 
-# This is a short explanation for a better understanding:
+# This is a short explanatGPIOn for a better understanding:
 # motor         -> which motor is selected left motor or right motor
-# mode          -> mode explains what action should be performed by the H-Bridge
+# mode          -> mode explains what actGPIOn should be performed by the H-Bridge
 
-# setMotorMode(motor1, reverse)      -> The left motor is called by a function and set into reverse mode
-# setMotorMode(motor2, stopp)       -> The right motor is called by a function and set into stopp mode
+# setMotorMode(motor1, reverse)      -> The left motor is called by a functGPIOn and set into reverse mode
+# setMotorMode(motor2, stopp)       -> The right motor is called by a functGPIOn and set into stopp mode
     def setMotorMode(self, motor, mode):
 
         if motor == "motor1":
             if mode == "reverse":
-                io.output(self.motor1_in1_pin, True)
-                io.output(self.motor1_in2_pin, False)
+                GPIO.output(self.motor1_in1_pin, True)
+                GPIO.output(self.motor1_in2_pin, False)
             elif  mode == "forward":
-                io.output(self.motor1_in1_pin, False)
-                io.output(self.motor1_in2_pin, True)
+                GPIO.output(self.motor1_in1_pin, False)
+                GPIO.output(self.motor1_in2_pin, True)
             else:
-                io.output(self.motor1_in1_pin, False)
-                io.output(self.motor1_in2_pin, False)
+                GPIO.output(self.motor1_in1_pin, False)
+                GPIO.output(self.motor1_in2_pin, False)
 
         elif motor == "motor2":
             if mode == "reverse":
-                io.output(self.motor2_in1_pin, False)
-                io.output(self.motor2_in2_pin, True)
+                GPIO.output(self.motor2_in1_pin, False)
+                GPIO.output(self.motor2_in2_pin, True)
             elif  mode == "forward":
-                io.output(self.motor2_in1_pin, True)
-                io.output(self.motor2_in2_pin, False)
+                GPIO.output(self.motor2_in1_pin, True)
+                GPIO.output(self.motor2_in2_pin, False)
             else:
-                io.output(self.motor2_in1_pin, False)
-                io.output(self.motor2_in2_pin, False)
+                GPIO.output(self.motor2_in1_pin, False)
+                GPIO.output(self.motor2_in2_pin, False)
         else:
-            self.resetMotorGPIO()
+            self.resetMotorGPGPIO()
 
 # SetMotorLeft(power)
 
 # Sets the drive level for the left motor, from +1 (max) to -1 (min).
 
-# This is a short explanation for a better understanding:
+# This is a short explanatGPIOn for a better understanding:
 # SetMotorLeft(0)     -> left motor is stopped
 # SetMotorLeft(0.75)  -> left motor moving forward at 75% power
 # SetMotorLeft(-0.5)  -> left motor moving reverse at 50% power
 # SetMotorLeft(1)     -> left motor moving forward at 100% power
-    def setMotorLeft(self, power):
+    def setMotorRun(self, power):
         if power < 0:
             # Reverse mode for the left motor
             self.setMotorMode("motor1", "reverse")
-            pwm = -int(self.PWM_MAX * power)
+            pwm = -self.PWM_MAX * power
             if pwm > self.PWM_MAX:
                 pwm = self.PWM_MAX
         elif power > 0:
             # Forward mode for the left motor
             self.setMotorMode("motor1", "forward")
-            pwm = int(self.PWM_MAX * power)
+            pwm = self.PWM_MAX * power
             if pwm > self.PWM_MAX:
                 pwm = self.PWM_MAX
+                print(pwm)
         else:
             # Stopp mode for the left motor
             self.setMotorMode("motor1", "stopp")
             pwm = 0
-#       print("SetMotorLeft", pwm)
+#       print "SetMotorLeft", pwm
         self.motor1pwm.ChangeDutyCycle(pwm)
 
 # SetMotorRight(power)
 
 # Sets the drive level for the right motor, from +1 (max) to -1 (min).
 
-# This is a short explanation for a better understanding:
+# This is a short explanatGPIOn for a better understanding:
 # SetMotorRight(0)     -> right motor is stopped
 # SetMotorRight(0.75)  -> right motor moving forward at 75% power
 # SetMotorRight(-0.5)  -> right motor moving reverse at 50% power
 # SetMotorRight(1)     -> right motor moving forward at 100% power
-    def setMotorRight(self, power):
+    def setMotorArm(self, power):
         if power < 0:
             # Reverse mode for the right motor
             self.setMotorMode("motor2", "reverse")
-            pwm = -int(self.PWM_MAX * power)
+            pwm = -self.PWM_MAX * power
             if pwm > self.PWM_MAX:
                 pwm = self.PWM_MAX
         elif power > 0:
             # Forward mode for the right motor
             self.setMotorMode("motor2", "forward")
-            pwm = int(self.PWM_MAX * power)
+            pwm = self.PWM_MAX * power
             if pwm > self.PWM_MAX:
                 pwm = self.PWM_MAX
         else:
@@ -154,8 +165,21 @@ class HBridge(object):
         #print "SetMotorRight", pwm
         self.motor2pwm.ChangeDutyCycle(pwm)
 
-# Program will clean up all GPIO settings and terminates
+    def setMotorSteer(self, power):
+        if power < 0:
+            pwm = power + self.CENTER
+            if pwm < self.LEFT_MAX:
+                pwm = self.LEFT_MAX
+        elif power > 0:
+            pwm = power + self.CENTER
+            if pwm > self.RIGHT_MAX:
+                pwm = self.RIGHT_MAX
+        else:
+            pwm = self.CENTER
+        self.steerpwm.ChangeDutyCycle(pwm)
+
+# Program will clean up all GPGPIO settings and terminates
     def exit(self):
         self.resetMotorGPIO()
-        io.cleanup()
+        GPIO.cleanup()
 
