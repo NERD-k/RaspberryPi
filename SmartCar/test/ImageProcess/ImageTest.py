9e5b12215075dc6e19fec8fe9d47c89e43b66879
孔NERD <smallnerd.k@gmail.com>
2018-07-07 17:22:26 +0800

    暂存，更换编程使用的Linux发行版

---
+++ SmartCar/test/ImageProcess/ImageTest.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Author:       孔NERD
+# Email:        smallnerd.k@gmail.com
+# Date:         2018-7-5
+
+from io import BytesIO
+from picamera import PiCamera
+from PIL import Image
+import time
+from L298NHBridge import HBridge
+
+IS_TEST = True
+DEBUG = False
+if IS_TEST:
+    DEBUG = True
+    LOG_PATH = '/home/pi/smartcar.log')
+
+def log(*args, **kwargs):
+    s = ' '.join(args)
+    s += ''
+    print(s)
+    if DEBUG:
+        with open(LOG_PATH, 'a') as f:
+            try:
+                f.write(s)
+                f.write('\n')
+            except:
+                f.write('log failed.' + '\n')
+
+def Init(self):
+    Motors = HBridge(19, 26, 23, 24, 13, 21, 22)
+    speed_run = 0
+    angle_steer = 0
+
+
+def Image(self):
+    with PiCamera() as camera:
+        stream = BytesIO()
+        camera.capture_continuous(stream, format='png'):
+            stream.truncate()
+            strea.seek(0)
+
+            image = Image.open(stream)
+            r, g, b = image.spilt()
+            r.save('/home/pi/Image/Image{counter}')
+ 
+            stream.truncate()
+            stream.seek(0)
+
+def main():
+    Init(self)
+    while True:
+        try:
+            pass
+        except:
+            print("Error, out of main loop!")
+
+if __name__ = '__main__':
+    main()
+
